21c9463600f66e9c8adfa588243f742cab21b927
Dawson Botsford <DawsonBotsford@gmail.com>
2017-03-31 11:16:19 -0700

    test each element in hackathon response

--- test.js
+++ test.js
@@ -4,17 +4,29 @@ import fetchEvents from './fetch-events';
 
 const buildMLHUrl = season => urlJoin('https://mlh.io/seasons/', season, '/events');
 
-test('f2015', async t => {
+test('f2015 responds with object', async t => {
   const f2015Response = await fetchEvents(buildMLHUrl('f2015'));
   t.notDeepEqual(f2015Response, []);
   t.is(typeof f2015Response, 'object');
 });
 
-test('na-2017', async t => {
+test('na-2017 contains valid hackathon data', async t => {
   const na2017Response = await fetchEvents(buildMLHUrl('na-2017'));
-  t.false(na2017Response[0].isHighSchool); // HackMTY
+
+  const HackMTY = na2017Response[0];
+  t.true(HackMTY.startDate === '2016-08-27');
+  t.true(HackMTY.endDate === '2016-08-28');
+  t.false(HackMTY.isHighSchool);
+
   t.true(na2017Response[26].isHighSchool); // GlassHacks
-  // HackMTY Dates
-  t.true(na2017Response[0].startDate === '2016-08-27');
-  t.true(na2017Response[0].endDate === '2016-08-28');
+
+  // verify data types of every hackathon in array
+  na2017Response.forEach(hackathon => {
+    t.is(typeof hackathon.name, 'string');
+    t.is(typeof hackathon.url, 'string');
+    t.is(typeof hackathon.startDate, 'string');
+    t.is(typeof hackathon.endDate, 'string');
+    t.is(typeof hackathon.location, 'string');
+    t.is(typeof hackathon.isHighSchool, 'boolean');
+  });
 });
